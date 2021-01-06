e7eb5717d60811d4269e3d6c51f1ee84a79f2da2
Ankit Sharma <56289073+AnkitSharma29@users.noreply.github.com>
2021-01-06 14:23:55 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/healthapi,{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/healthapi',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
